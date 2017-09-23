0b974b4a229993c3ee47c5507af3569a5894f372
Simon Katz <sk@nomistech.com>
2017-09-23 08:37:47 +0100

    Version 0.2.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomis-zzzz-play-library "0.2.7"
+(defproject nomis-zzzz-play-library "0.2.8-SNAPSHOT"
   :description "A library for playing with things -- eg for testing deployment to Clojars."
   :url "https://github.com/simon-katz/nomis-zzzz-play-library"
   :license {:name "Eclipse Public License"
